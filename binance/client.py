55b9308ac6369742f817f2f7fba9e51c6dab579a
Vladyslav Isakov <PR0PH3CY0x1@proton.me>
2024-03-29 23:34:15 +0200

    Parser

---
+++ binance/client.py
@@ -0,0 +1,58 @@
+from binance.client import Client
+
+
+class BinanceClient:
+    def __init__(
+            self, crypto, 
+            api, secret):
+        self.crypto = crypto
+
+        self.client = Client(
+            api_key=api,
+            api_secret=secret,
+            testnet=False
+        )
+
+
+    def get_balance(self):
+        self.data = self.client.get_account()
+
+
+    def list_assets(self):
+        assets = {
+
+        }
+
+        for asset in self.data['balances']:
+            if (asset['asset'] in self.crypto
+                and asset['asset'] not in ['USDT', 'USDC']):
+                _price = float(self.client.get_ticker(
+                    symbol=f'{asset["asset"]}USDT'
+                )['lastPrice'])
+
+                _name = str(asset['asset'])
+
+                _amount = float(asset['free'])
+
+                _present = round(
+                    float(_amount * _price),
+                    3
+                )
+
+                assets[_name] = {
+                    'amount': _amount,
+                    'price': _price,
+                    'present': _present
+                }
+
+            elif (asset['asset'] in ['USDT', 'USDC']
+                and float(asset['free']) != 0.0):
+                _name = str(asset['asset'])
+
+                _amount = float(asset['free'])
+
+                assets[_name] = {
+                    'amount': _amount
+                }
+
+        return assets
---
+++ shared/config.py
@@ -0,0 +1,18 @@
+from configparser import ConfigParser
+
+
+class Config:
+    def __init__(self, path):
+
+        self.config = path
+        self.parser = ConfigParser()
+
+    def read(self):
+
+        self.parser.sections()
+
+        self.parser.read(
+            self.config
+        )
+
+        return self.parser
